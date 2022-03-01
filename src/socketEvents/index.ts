137efd5357dd95dc51258d6c271ddc88b5513820
DESKTOP-98AMS46\Amin <972839577@qq.com>
2022-03-01 13:20:06 +0800

    Complete enter username.

--- src/socketEvents/index.ts
+++ src/socketEvents/index.ts
@@ -1,7 +1,8 @@
 import { Socket } from 'socket.io-client';
 import { nameIsDuplicate } from './nameIsDuplicate';
+import { canUseUsername } from './canUseUsername';
 
-const arr = [nameIsDuplicate];
+const arr = [nameIsDuplicate, canUseUsername];
 
 export function initSocketEvents(socket: Socket) {
     arr.forEach((fn: (socket: Socket) => void) => fn(socket));
